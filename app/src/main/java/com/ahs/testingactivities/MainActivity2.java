e331aa9ea8047809e530f07bec215bdf2a2602cf
Asad Al Badi <deava0@gmail.com>
2021-04-28 02:19:31 +0400

    Add Book Activity + Pic From Gallery

--- app/src/main/java/com/ahs/testingactivities/MainActivity2.java
+++ app/src/main/java/com/ahs/testingactivities/MainActivity2.java
@@ -1,14 +1,80 @@
 package com.ahs.testingactivities;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.koushikdutta.ion.Ion;
+
+import java.util.NoSuchElementException;
 
 public class MainActivity2 extends AppCompatActivity {
+    ImageView ivbookPic1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        ivbookPic1 = (ImageView) findViewById(R.id.ivbookPic);
+        ivbookPic1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Toast.makeText(MainActivity2.this, "Choose a Books' Cover", Toast.LENGTH_LONG).show();
+                //get image from gallery
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto , 1);
+
+            }
+        });
+
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case 0 :
+            case 1:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    ivbookPic1.setImageURI(selectedImage);
+                }
+
+                break;
+        }
+    }
+
+    //ignore
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ivbookPic: {
+
+            }
+        }
+
+
+    }
+
 }
+
+
+
